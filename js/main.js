97a299b753d38dbe7c5912dac965de05af21b913
@juanwmedia <juan@wmedia.es>
2016-07-11 15:01:58 +0200

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: 'body',
+    data: {
+        nuevaTarea: null,
+        editandoTarea: null,
+        tareas: [
+            {titulo: 'Salir a correr', completado: false},
+            {titulo: 'Ir al gimnasio', completado: false},
+            {titulo: 'Limpiar el coche', completado: false},
+            {titulo: 'Hacer la compra', completado: false},
+            {titulo: 'Aprender VueJS & Firebase', completado: false},
+        ]
+    },
+    methods: {
+        agregarTarea: function(tarea) {
+            this.tareas.unshift({
+               titulo: tarea, completado: false
+            });
+            this.nuevaTarea = '';
+        },
+        editarTarea: function(tarea) {
+          console.info(tarea);
+        },
+        eliminarTarea: function(indice) {
+            this.tareas.splice(indice, 1);
+        },
+    }
+});
